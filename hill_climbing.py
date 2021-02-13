d0ea108f3e602b9d7739d023f9afe0f810ab6261
Dilixiati Yimuran <Emran.dilixiati@gmail.com>
2021-02-13 13:50:18 +0100

    Hill Climbing 98%

--- hill_climbing.py
+++ hill_climbing.py
@@ -4,11 +4,11 @@ import copy
 #Given a route, finds the distance of the route
 def search(List, route):
     distance = 0
-    for i in range(1, len(route)):
+    for i in range(len(route)):
         distance += List[route[i-1]][route[i]]
     return distance
 
-def swap1(List, route):
+def swap1(route):
     Routes = [route]
     for i in range(1, len(route)-1):
         for j in range(i+1, len(route)):
@@ -19,38 +19,51 @@ def swap1(List, route):
             Routes.append(tempRoute)
     return Routes
 
-with open("european_cities.csv", "r") as file:
-    cityNames = file.readline().split(";")
+def execute():
+    with open("european_cities.csv", "r") as file:
+        cityNames = file.readline().split(";")
 
-    List = []
-    
-    for line in file:
-        tempLine = line.split(";")
-        for i in range(len(tempLine)):
-            tempLine[i] = float(tempLine[i])
-        List.append(tempLine)
+        List = []
+        for line in file:
+            tempLine = line.split(";")
+            for i in range(len(tempLine)):
+                tempLine[i] = float(tempLine[i])
+            List.append(tempLine)
+            
+        routeLength = len(List)
+        randomRoute = [i for i in range(routeLength)]
+        np.random.shuffle(randomRoute)
 
-    routeLength = len(List) - 13
-    randomRoute = [i for i in range(routeLength)]
-    np.random.shuffle(randomRoute)
+        #print(randomRoute)
+        first = search(List, randomRoute)
+        neighRoutes = swap1(randomRoute)
+        routesDistance = [search(List, neighRoutes[i]) for i in range(len(neighRoutes))]
+        
+        min_dis = min(routesDistance)
+        Best = routesDistance.index(min_dis)
+        P = neighRoutes[Best]
 
-    neighRoutes = swap1(List, randomRoute)
-    routesDistance = [search(List, neighRoutes[i]) for i in range(len(neighRoutes))]
+        while min_dis < first: 
+            first = min_dis 
+            neighRoutes = swap1(P)
+            routesDistance = [search(List, neighRoutes[i]) for i in range(len(neighRoutes))]
+            
+            min_dis = min(routesDistance)
+            Best = routesDistance.index(min_dis)
+            P = neighRoutes[Best]
 
-    min_dis = min(routesDistance)
-    Best = routesDistance.index(min_dis)
-    P = neighRoutes[Best]
+    return min_dis, [cityNames[P[i]] for i in range(len(P))]
 
+if __name__ == "__main__": 
+    Distance = []
+    Routes = []
 
     for i in range(20):
-        neighRoutes = swap1(List, P)
-        routesDistance = [search(List, neighRoutes[i]) for i in range(len(neighRoutes))] 
-        min_dis = min(routesDistance)
-
-
-    #print(min_dis)
-
-    """
-    print(min_dis)
-    print( [cityNames[P[i]] for i in range(len(P))] )
-    """
+        min_dis, route = execute()
+        Distance.append(min_dis)
+        Routes.append(route)
+    
+    min_dis = min(Distance)
+    T = Distance.index(min_dis)
+    P = Routes[T]
+    print(min_dis, P)
